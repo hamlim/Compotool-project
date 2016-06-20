7cf3ff38c64bd9997f8ab8b995e3fa1908465735
hamlim <hamlim@rpi.edu>
2016-06-20 14:56:15 -0700

    starting js code

---
+++ Dev/assets/js/prod/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // variables
+    // inputs
+    // Units
+    var unit_metric_elem = document.getElementByClassName('units--metric-input')[0];
+    var unit_imperial_elem = document.getElementByClassName('units-imperial-inpu')[0];
+    // Block up weight + number of boards -> buw
+    var buw_ct300_volume_elem = document.getElementById('input--buw_nob-1');
+    var buw_ct8504_volume_elem = document.getElementById('input--buw_nob-2');
+    var buw_ct8502_colume_elem = document.getElementById('input--buw_nob-3');
+    
+});
---
+++ Dev/assets/js/src/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // variables
+    // inputs
+    // Units
+    let unit_metric_elem = document.getElementByClassName('units--metric-input')[0];
+    let unit_imperial_elem = document.getElementByClassName('units-imperial-inpu')[0];
+    // Block up weight + number of boards -> buw
+    let buw_ct300_volume_elem = document.getElementById('input--buw_nob-1');
+    let buw_ct8504_volume_elem = document.getElementById('input--buw_nob-2');
+    let buw_ct8502_colume_elem = document.getElementById('input--buw_nob-3');
+
+});
